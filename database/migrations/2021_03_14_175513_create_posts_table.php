6a7e35aa5ff2105290b895c5bafba1623bb8690b
Muathye <muath.ye@gmail.com>
2021-03-14 22:39:17 +0300

    migration

--- database/migrations/2021_03_14_175513_create_posts_table.php
+++ database/migrations/2021_03_14_175513_create_posts_table.php
@@ -15,6 +15,8 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
