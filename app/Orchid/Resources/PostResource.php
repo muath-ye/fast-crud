092b5a75e5f0d713f57dd00359cbc7fcf927bdec
Muathye <muath.ye@gmail.com>
2021-03-14 21:03:59 +0300

    add orchid post resource

---
+++ app/Orchid/Resources/PostResource.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Orchid\Resources;
+
+use Orchid\Crud\Resource;
+use Orchid\Screen\TD;
+
+class PostResource extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = '';
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @return array
+     */
+    public function fields(): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the columns displayed by the resource.
+     *
+     * @return TD[]
+     */
+    public function columns(): array
+    {
+        return [
+            TD::make('id'),
+
+            TD::make('created_at', 'Date of creation')
+                ->render(function ($model) {
+                    return $model->created_at->toDateTimeString();
+                }),
+
+            TD::make('updated_at', 'Update date')
+                ->render(function ($model) {
+                    return $model->updated_at->toDateTimeString();
+                }),
+        ];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @return array
+     */
+    public function filters(): array
+    {
+        return [];
+    }
+}
